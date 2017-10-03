4209ff57bfa7b311698a6a4f19cd543f734fc529
Joao Ribeiro <31766531+Loosemond@users.noreply.github.com>
2017-10-03 22:41:57 +0100

    Rename main.asm to Led_1.asm
    Trabalho 1

--- 2DD_melhorgrupo/Led_1.asm
+++ 2DD_melhorgrupo/Led_1.asm
@@ -11,27 +11,27 @@
 
 .cseg ; reset de vector
 .org 0x0 ;defina a o sitio na memoria
-		jmp main ;começa o programa na func main 
+		jmp main ;comeÃ§a o programa na func main 
 .cseg
-.org 0x46 ; estamos a deixar espaço para adicionarmos mais codigo antes da execução do prog
-;------------------------------Inicialização----------------------------
+.org 0x46 ; estamos a deixar espaÃ§o para adicionarmos mais codigo antes da execuÃ§Ã£o do prog
+;------------------------------InicializaÃ§Ã£o----------------------------
 inic:		ldi r16,0b11111111;		vai servir para configurar as saidas e as entradas, temos de escrever todas a portas do PORTA  de uma vez so dai temos 8.
-			out DDRA,r16 ;			aqui é que se passa os valor em r16 para o PORTA ????
+			out DDRA,r16 ;			aqui Ã© que se passa os valor em r16 para o PORTA ????
 
 			;ldi r16,0b11111111; Vai indicar que os led tao desligados pois 1 representa off
-			out DDRC,r16		;DDRC índica nos que é o PORTC
+			out DDRC,r16		;DDRC Ã­ndica nos que Ã© o PORTC
 			ldi r17,0b00000000	;limpa o r17
 
-			ret						; Indica o fim da funçao e vai pra a linha assegir de Call inic
+			ret						; Indica o fim da funÃ§ao e vai pra a linha assegir de Call inic
 
 ;---------------------------Programa Principal--------------------------
 main:
-		ldi		r16,0xff			;Deste modo escreve na ram de baixo para cima .  spl e sph servem para escrever o endereço 0x10ff num sistema em que so temos 8bits. 
+		ldi		r16,0xff			;Deste modo escreve na ram de baixo para cima .  spl e sph servem para escrever o endereÃ§o 0x10ff num sistema em que so temos 8bits. 
 		out		spl,r16
 		ldi		r16,0x10
 		out		sph,r16
 
-		call	inic				; É como se fosse uma funçao vai para a primeira linha do inic  
+		call	inic				; Ã‰ como se fosse uma funÃ§ao vai para a primeira linha do inic  
 
 
 						
