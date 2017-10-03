40d1fac1ad0294b858fdf83e1fc31775e7b7bb3d
Joao Ribeiro <31766531+Loosemond@users.noreply.github.com>
2017-10-03 20:14:21 +0100

    reorganisar os ficheiros

--- 2DD_melhorgrupo/main.asm
+++ 2DD_melhorgrupo/main.asm
@@ -32,18 +32,38 @@ main:
 		out		sph,r16
 
 		call	inic				; É como se fosse uma funçao vai para a primeira linha do inic  
+
+
+						
 ciclo:
 		in		r16,PINA			;le PORTA para r16
-		andi	r16,0b00000001		; ??	
-		cpi		r16,0b00000001		; Compara o que esta em r16 com o valor que colocamos  se for igual salta a linha.
+		;andi	r16,0b00000001		; ??	
+		;cpi		r16,0b00000001		; Compara o que esta em r16 com o valor que colocamos  se for igual salta a linha.
 
-		brne	fim					;
+		;brne	fim					;
 
 
-		ldi		r16,0b00001100 ;  Liga o led 3 e o 4
+		;ldi		r16,0b00001000 ;  Liga o led 3 e o 4
+		lsl		r16 ;vai para a esquerda 
+		lsl		r16
+		lsl		r16
+		lsl		r16
 		out		PORTC,r16
+		;mov		r17,r16	; move os r
 
-	
+		
+		call	inver
+		add		r16,r18
+		out		PORTC,r16
+		;ror		r17	;roda para a direita
+		;add		r16,r17
+		
+		;out		PORTC,r16
+		;ldi		r18,0b01000000
+		;out		PORTC,r18
+		;ror		r18
+		;asr		r18
+		;out		PORTC,r18
 
 		jmp		ciclo
 
@@ -53,7 +73,36 @@ fim:
 		
 		jmp		ciclo
 		
-		
 
+
+
+Inver:	
+		ldi		r18,0b00000000
+		bst		r16,0	;faz load do bit do r para T
+		bld		r18,7	; escreve o bit de T em r18	
+				
+		bst		r16,1	
+		bld		r18,6
+
+		bst		r16,2	
+		bld		r18,5
+
+		bst		r16,3	
+		bld		r18,4
+
+		bst		r16,4	
+		bld		r18,3
+
+		bst		r16,5	
+		bld		r18,2
+
+		bst		r16,6	
+		bld		r18,1
+
+		bst		r16,7	
+		bld		r18,0
+
+		ret
+
+inver2:
 		
-			 
