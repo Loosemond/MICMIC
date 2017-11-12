1520d185be01af88191c17e9cd23b35a6ae5aaa6
Joao Ribeiro <31766531+Loosemond@users.noreply.github.com>
2017-11-12 15:45:21 +0000

    para um digito de cada vez
    valta agora parar sosinho consoante o tempo que o utilizador demorou a carregar no botao

--- 2DD_melhorgrupo/microsolt_3disp.asm
+++ 2DD_melhorgrupo/microsolt_3disp.asm
@@ -27,7 +27,7 @@
 
 .def		contador = r18	; muda o nome
 
-.equ		tempo1 =5
+.equ		tempo1 =5;5
 
 .equ		zero	= 0xC0
 
@@ -49,8 +49,8 @@
 
 .equ		nove	= 0x90
 
-.equ        ap	= 0xff
-
+.equ        	ap	= 0xff
+.equ		m_p1	= 0x1b
 .equ		delay	=200 ;mudar para 100
 
 .cseg ; reset de vector
@@ -92,20 +92,33 @@ inic:
 			;timers--------------------------------
 			
 
-			ldi temp,4  ;1ms mudar para 124
-			out ocr0,temp		; é o valor que maximo que o contador conta
+			ldi 	temp,124  ;1ms mudar para 124
+			out 	ocr0,temp		; é o valor que maximo que o contador conta
 
 			ldi	cnt_int,tempo1	;contador de 5ms
 
-			clr temp  ; se tiver as 0 esta parado
-			out tccr0,temp
+			;clr 	temp  ; se tiver as 0 esta parado
+			;out 	tccr0,temp
 
-			in r16,TIMSK	;activa a interrupçao do tc0
-			ori r16,0b00000010
-			out timsk,r16
- 			ldi		temp,0b00001101
-			out		tccr0,temp
-			bset		6			
+			in 	r16,TIMSK	;activa a interrupçao do tc0
+			ori 	r16,0b00000010
+			out 	timsk,r16
+			
+			
+ 			ldi	temp,0b00001101
+			out	tccr0,temp
+			;bset		6
+			;timer2
+			;ldi	temp,0x09
+			;out	ocrbl,temp
+			;ldi	temp,0x3D
+			;out	ocrbh,temp
+			;clr 	temp  ;
+			;out 	tccrnb,temp
+			
+			;in 	r16,TIMSK	;activa a interrupçao do tc0
+			;ori 	r16,0b00000010
+			;out 	timsk,r16
 			;------RAM--------------------------------------------
 			ldi	zl,low(table*2)
 			ldi 	zh,high(table*2)
@@ -127,17 +140,21 @@ inic:
 			ldi	yl,0x20 ;reset ao ponteiro y
 			ldi	r19,0			
 			st	x+,r19	;fica tudo a zero
+			ldi	r19,10	;deste modo os displays ficam desligados
 			st	x+,r19
 			st	x+,r19			
 			;---------Memoria que indica se é para incrementar ou nao
 			ldi	xl,0x1d
 			ldi	r19,1 ; 1 representa que é para incrementar
 			st	x+,r19
-			st	x+,r19
-			ldi	r19,1
+			ldi	r19,0	;deste modo so incrementa o 1o
+			st	x+,r19			
 			st	x,r19
-			
-			;--------
+			;----------programa1(parar_dig)---------
+			ldi	xl,m_p1
+			ldi	r19,0x1d
+			st	x,r19
+			;---------------------------------------
 			ldi	xl,00 ; reset do ponteiro
 
 			;-----------------------------------------------------------------------
@@ -182,26 +199,29 @@ cicloini0:
 			
 			jmp		cicloini0
 			;jmp		int_int0
-numerosv3:		
+numerosv3:		;--------display----------------------
 			push		r16
 			push		r22
 			ldi		r22,0x20			
-tag2:			cpi		yl,0x23
+			cpi		yl,0x23
 			breq		reset5
 tag1:			LD		r16,y+
 			OUT		PORTD,r16
-			;------------
+			;----
 			sub		yl,r22 ; assim so uso um apontador
 			LD		contador,y
 			add		zl,contador		;soma o numero que se vai querer colocar no display 			
 			lpm		display,z		;vai ao local da memoria e carrega o o valor que la estiver
 			out		PORTC,display	;
 			ldi		zl,low(table*2) ; COLOCA O APONTADOR DA MEMORIA EM ZERO	
-			add		yl,r22
-		
+			add		yl,r22		
 			pop		r22
 			pop		r16
 			ret	
+reset5:					
+			ldi		yl,0x20
+			jmp		tag1			
+			;----------------------------------------
 int_int0:	
 			push		r16
 			mov		r16,flag
@@ -217,30 +237,53 @@ int_int1:
 			
 			
 			push		r16
+			
 			mov		r16,flag
-			sbr		r16,0b00000001
-			mov		flag,r16
+			;sbr		r16,0b00000001		
+			mov		flag,r16		;activa a flag1				
+			call		parar_dig		
 			pop		r16
 			reti
 
+parar_dig:		;---------------para um digito e mete outro a rolar---------
+			push		r16
+			push		r17
+			push		r18
+			mov		r18,yl						
+			ldi		yl,m_p1			;m_p1 é onde guardo a posiçao da memoria que quero ler neste programa ou seja é uma variavel	
+			ld		r16,y			;guardo um valor no r17			
+			mov		yl,r16			;carrega o valor que estava guardado no m_p1 que era a posiçao de memoria guardada			
+			ldi		r16,0
+			st		y+,r16			;aqui vai escrever de modo a que o display actual pare de contar e o seguinte se ligue e que começe a contar
+			ldi		r16,1
+			ldi		r17,0x20		;preciso de outro registo pois o cpse so compara se tiver 2 registos e como so da skip a uma linha nao dava pois preciso de fazer o ldi do r16 antes de fazer o ld
+			cpse		yl,r17			;Aqui verifico se ja parei o 3 digito se sim entao nao é preciso escrever mais nada alias se deixar escrever ele vai escrever por cima da memoria que indica qual display representa o digito1
+			st		y,r16			;indica que vai incrementar
+			mov		r16,yl			;guardo a posiçao actual num registo
+			ldi		yl,m_p1			;aponto para a variavel m_p1
+			st		y,r16			;gravo na varivel m_p1 a posiçao em que fiquei neste cilco
+			mov		yl,r18			;volata a apontar para a posiçao que tinha antes de entrar neste programa 
+			pop		r18
+			pop		r17
+			pop		r16
+			ret
+			;-----------------------------------------------------------
 int_tc0:	
 			dec		cnt_int
 			brne		f_int			; verifica se é 0
-			ldi		cnt_int,tempo1
+			ldi		cnt_int,tempo1		;rest ao contador de 5ms
 			
-			call		numerosv3	
+			call		numerosv3		;mostra os numeros
 			push		r22
 			mov		r22,flag
-			sbrs		r22,0
-			call		incre			 
-			pop		r22		
-			;brtc	salto1 ; caso nao tenha carregado para parar vai para o salto 1
-			;dec     timer2
-			;brne	reset3
-			reti
-			;set					;activa a flag t escrever a qui o codigo
-
-salto1:		
+			sbrs		r22,0			;verifica se ja carregamos no butao para começar
+			call		modo1					 
+			pop		r22					
+			reti			
+Modo1:			
+			call		incre
+			
+			ret
 			;-------------incrementa---------------
 incre:			;pop		r22;VEM DE TRAS
 			push		r16
@@ -261,9 +304,9 @@ incre:			;pop		r22;VEM DE TRAS
 			ld		r16,y
 			inc		r16
 			cpi		r16,10
-			breq		increreset			
+			brge		increreset	; deste modo garanto que fasso reset ao numero mesmo que este seja maior que 10		
 tag3:			st		y,r16
-skip0:			add		r17,r18		; deste modo asseguro que acabom com o apontador em 0x20 pois se saltar so preciso de sumar 4 (r18 vai ser 0 logo isto da)			
+skip0:			add		r17,r18		; deste modo asseguro que acabo com o apontador em 0x20 pois se saltar so preciso de sumar 4 (r18 vai ser 0 logo isto da)			
 			add		yl,r17
 			pop		r18
 			pop		r17
@@ -282,10 +325,7 @@ f_int:
 
 
 
-reset5:		
-			;st		y,r22   ;rest do apontador
-			ldi		yl,0x20
-			jmp		tag1
+
 
 reset:			ldi		contador,0
 			reti
